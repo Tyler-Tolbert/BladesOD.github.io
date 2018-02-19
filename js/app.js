3597c586877407cec171aafb009164d730ffc133
Tyuiodragon <tylertolbert@yahoo.com>
2018-02-18 21:58:58 -0600

    first commit
    adding website

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$('.first').addClass('zoomIn animated');
+
+setTimeout(function () {
+    $('.second').show().addClass('fadeIn animated');}, 0
+);
+
+setTimeout(function () {
+    $('.third').show().addClass('fadeInUp animated');}, 1000
+);
+
+
+
+var coverimg1 = $('#coverimg1')	
+
+setTimeout( function() {
+	$("images[src='../images/ffxivPIC1.jpg']").addClass('fadeIn animated'); 1000
+})
